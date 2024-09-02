fdab395e83639ca7a3dc20b30918c34e7f4314ae
Gabriel-Schumacher <schumachergabe@gmail.com>
2024-09-01 19:49:03 -0600

    Made it so you can see list

--- dataTypes.js
+++ dataTypes.js
@@ -1,7 +1,20 @@
+let toDos = []
+
 let todoTemplate = {
     id: 0,
     name: "",
     status: "",
     category: "",
     dueDate: "9-1-2024"
+}
+
+function editStatus(id, newStatus) {
+    const todo = toDos.find(todo => todo.id === id)
+    if (todo) {
+        todo.status = newStatus
+    }
+}
+
+function completeToDo(id) {
+    editStatus(id, "completed")
 }
---
+++ index.js
@@ -0,0 +1,42 @@
+let toDos = [
+    todoTemplate = {
+        id: 0,
+        name: "template",
+        status: "pending",
+        category: "None",
+        dueDate: "9-1-2024"
+    },
+    walkDog = {
+        id: 0,
+        name: "Walk the Dog",
+        status: "pending",
+        category: "Chores",
+        dueDate: "9-1-2024"
+    }
+]
+
+function editStatus(id, newStatus) {
+    const todo = toDos.find(todo => todo.id === id)
+    if (todo) {
+        todo.status = newStatus
+    }
+}
+
+function completeToDo(id) {
+    editStatus(id, "completed")
+}
+
+console.log("Hello World")
+
+function showList() {
+    const toDoList = document.getElementById('toDoList')
+    toDoList.innerHTML = "",
+    toDos.forEach(todo => {
+        let listItem = document.createElement('li')
+        listItem.textContent = `${todo.name} - ${todo.status} - ${todo.category} - Due: ${todo.dueDate}`
+        listItem.classList.add('toDoItem', todo.status)
+        toDoList.appendChild(listItem)
+    })
+}
+
+showList()
