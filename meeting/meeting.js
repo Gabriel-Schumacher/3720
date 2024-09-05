e9a869b3c70eb56832ec1e4ba9e9159cbeb8a920
Gabriel-Schumacher <schumachergabe@gmail.com>
2024-09-05 13:50:30 -0600

    played with primitive values

---
+++ meeting/meeting.js
@@ -0,0 +1,9 @@
+console.log('hello world')
+
+
+//let isSad = true;
+//let isHappy = !isSad; // The opposite
+//let isFeeling = isSad || isHappy; // Is at least one of them true?
+//let isConfusing = isSad && isHappy; // Are both true?
+
+let eels = 2 + 2
