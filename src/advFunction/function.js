147f48412b6e961148861b4fe85382f572906795
Gabriel-Schumacher <schumachergabe@gmail.com>
2024-09-24 17:05:45 -0600

    Followed along in class

---
+++ src/advFunction/function.js
@@ -0,0 +1,71 @@
+let countDown = (num) => {
+    //console.log(num)
+
+    if (num === 0) return
+    countDown(num -1)
+}
+
+countDown(3)
+
+let factorial = (num) => {
+    //console.log(num)
+    if (num === 1) return 1
+    return num * factorial(num - 1) 
+}
+
+let myNum = factorial(4)
+
+//console.log(myNum)
+
+let reverse = (string) => {
+    //console.log(string[0])
+    //console.log(string.substring(1))
+
+    if (string === '') return ''
+    return reverse(string.substring(1)) + string[0]
+}
+
+
+
+//console.log(reverse('train'))
+
+//console.log(reverse('frankenstein'))
+
+let company = {
+    sales: [{
+        name: 'john',
+        salary: 1000
+    },{
+        name: 'Alice',
+        salary: 1600
+    }],
+    development: {
+        sites: [{
+            name: 'Peter',
+            salary: 2000
+        },{
+            name: 'Alex',
+            salary: 1800
+        }],
+        internals: [{
+            name: 'Jack',
+            salary: 1300
+        }]
+    }
+}
+
+function countSalary (department) {
+    if (Array.isArray(department)) {
+        //return all salary summed together Case One
+        return department.reduce((prev, current) => prev + current.salary, 0)
+    } else {
+        let sum = 0
+        for(let subdep of Object.values(department)) {
+            //make recursive call Case two
+            sum += countSalary(subdep)
+        }
+        return sum
+    }
+}
+
+console.log(countSalary(company))
